d988752b346759748b51e5f3ced78438ae39711b
Carvalho-99 <brunocalves99@gmail.com>
2025-08-05 10:04:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnTopo = document.getElementById('btn-topo');
+
+  if (btnTopo) {
+    window.addEventListener('scroll', () => {
+      btnTopo.style.display = window.scrollY > 300 ? 'block' : 'none';
+    });
+
+    btnTopo.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
+});
